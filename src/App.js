f947b17b55a7fd9935fd1c78b288bfdebca09b16
TaylorDickens <justindickens17@gmail.com>
2020-03-26 17:41:49 -0400

    taylors branch

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <h1>Master Branch</h1>
       <h1>Develop Branch</h1>
+      <h1>Taylor Dickens</h1>
     </div>
   );
 }
